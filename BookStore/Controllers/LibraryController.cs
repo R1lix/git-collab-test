83faa6cc0771e7817d763aeedc6356738bcabca5
hitchh1ker <maks.shamov.99@mail.ru>
2025-09-13 14:51:32 +0500

    new feature for max

---
+++ BookStore/Controllers/LibraryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        public async Task<ActionResult> TestMethod1()
+        {
+            var error = "ошибка доступа баляяя";
+            return Forbid(error);
+        }
+        public async Task<ActionResult> TestMethod2()
+        {
+            var error = "ошибка доступа баляяя";
+            return Forbid(error);
+        }
+        public async Task<ActionResult> TestMethod3()
+        {
+            var error = "ошибка доступа баляяя";
+            return Forbid(error);
+        }
+    }
+}
